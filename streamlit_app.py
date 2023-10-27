34c1b552b12f00d6ac73b37b72bb9c517044bb38
Clark Williamson <clarklwilliamson@gmail.com>
2023-10-27 15:15:49 -0700

    case

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,6 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-snow_fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_data_row.index), ['Avocado', 'Strawberries'])
+snow_fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_data_row.index), ['avocado', 'strawberry'])
 snow_fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(snow_fruits_to_show)
